1271e277faa21b7eb3b1eb3654b8df0ba4a7434a
andikscript <andikscript@gmail.com>
2022-05-27 23:13:38 +0700

    initial commit

---
+++ ImagePuzzle/FormPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagePuzzle
+{
+    public partial class FormPuzzle : Form
+    {
+        public FormPuzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
