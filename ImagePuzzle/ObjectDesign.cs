8d99719d2d8665a5bc584642ec1108c1446585e9
andikscript <andikscript@gmail.com>
2022-06-02 21:28:26 +0700

    add object for string design

---
+++ ImagePuzzle/ObjectDesign.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagePuzzle
+{
+    internal class ObjectDesign
+    {
+        public string labelTimeStart;
+        public string labelTimeEnd;
+        public string labelMovesMade;
+        public string buttonPaused;
+        public string buttonResume;
+
+        public ObjectDesign(string labelTimeStart, string labelTimeEnd, string labelMovesMade, string buttonPaused,
+            string buttonResume)
+        {
+            this.labelTimeStart = labelTimeStart;
+            this.labelTimeEnd = labelTimeEnd;
+            this.labelMovesMade = labelMovesMade;
+            this.buttonPaused = buttonPaused;
+            this.buttonResume = buttonResume;
+        }
+    }
+}
